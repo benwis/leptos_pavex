78a9cd368df59859d60bd625e6a854e433d41ca2
benwis <ben@celcyon.com>
2024-06-20 07:21:16 -0700

    Basic example

---
+++ examples/basic_pavex/app/src/routes/mod.rs
@@ -0,0 +1,11 @@
+pub mod greet;
+pub mod ping;
+
+use pavex::blueprint::{router::GET, Blueprint};
+use pavex::f;
+
+pub fn register(bp: &mut Blueprint) {
+    bp.route(GET, "/api/ping", f!(self::ping::get));
+    bp.route(GET, "/api/greet/:name", f!(self::greet::get));
+    bp.route(GET, "/", f!(leptos_pavex::render_route_with_context));
+}
---
+++ examples/basic_pavex/server/src/configuration/mod.rs
@@ -0,0 +1,5 @@
+mod profile;
+mod schema;
+pub use profile::ApplicationProfile;
+pub use schema::Config;
+pub use schema::ServerConfig;
