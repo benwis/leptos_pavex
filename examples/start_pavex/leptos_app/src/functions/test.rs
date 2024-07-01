caa266aaf084d70edf9138205e03a223a87c7f77
benwis <ben@celcyon.com>
2024-06-30 21:11:17 -0700

    Stuff

--- examples/start_pavex/leptos_app/src/functions/test.rs
+++ examples/start_pavex/leptos_app/src/functions/test.rs
@@ -1,5 +1,6 @@
-use leptos::prelude::*;
-
+// use leptos_pavex_macro::server;
+use leptos::prelude::server;
+use leptos::prelude::ServerFnError;
 #[server(endpoint="greet")]
 pub async fn greetings(name: String) -> Result<String, ServerFnError>{
     Ok(format!("Salutations {name}"))
